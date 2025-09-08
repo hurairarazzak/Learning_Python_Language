ac4c0786aa9f977365c2542f654dfedd5231cb7d
hurairarazzak <hurairarazzak125@gmail.com>
2025-09-08 23:44:07 +0500

    first commit

---
+++ Variable.py
@@ -0,0 +1,24 @@
+# Our first Program
+print("Hello World!")
+
+name = "Huraira Razzak"
+age = 25
+price = 25.99
+
+age1 = 15
+old = False
+ab = None
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+print(type(age1))
+print(type(old))
+print(type(ab))
+
+# Small Code Print SUM
+a = 1000
+b = 500
+sum = a - b
+print(sum)
