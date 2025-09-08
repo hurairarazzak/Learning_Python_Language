a581aaffb176f84c340497a6844fa42807c44eab
hurairarazzak <hurairarazzak125@gmail.com>
2025-09-09 00:28:17 +0500

    assignment and logical operator learned

--- Variable.py
+++ Variable.py
@@ -1,27 +1,27 @@
-# # Our first Program
-# print("Hello World!")
+# Our first Program
+print("Hello World!")
 
-# name = "Huraira Razzak"
-# age = 25
-# price = 25.99
+name = "Huraira Razzak"
+age = 25
+price = 25.99
 
-# age1 = 15
-# old = False
-# ab = None
+age1 = 15
+old = False
+ab = None
 
-# print(type(name))
-# print(type(age))
-# print(type(price))
+print(type(name))
+print(type(age))
+print(type(price))
 
-# print(type(age1))
-# print(type(old))
-# print(type(ab))
+print(type(age1))
+print(type(old))
+print(type(ab))
 
-# # Small Code Print SUM
-# a = 1000
-# b = 500
-# sum = a - b
-# print(sum)
+# Small Code Print SUM
+a = 1000
+b = 500
+sum = a - b
+print(sum)
 
 # arithmetic operators
 firstVal = 5
@@ -44,3 +44,20 @@ print(a >= b) # True bcz a is greater than b and that right statment
 print(a > b) # True
 print(a <= b) # False bcz a is less than equal to b
 print(a < b)  # False
+
+# assignment operators
+num = 10
+num **= 5
+print("num :", num)
+
+# logical Operators
+a = 50
+b = 20
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("AND operator: ", val1 and val2) 
+
+print("OR operator: ", (a == b) or (a > b)) 
