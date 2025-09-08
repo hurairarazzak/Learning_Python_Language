9804b29044c11f43932a12ec2a9c11e87bdae619
hurairarazzak <hurairarazzak125@gmail.com>
2025-09-08 23:59:48 +0500

    learn till comparison operators

--- Variable.py
+++ Variable.py
@@ -1,24 +1,46 @@
-# Our first Program
-print("Hello World!")
+# # Our first Program
+# print("Hello World!")
 
-name = "Huraira Razzak"
-age = 25
-price = 25.99
+# name = "Huraira Razzak"
+# age = 25
+# price = 25.99
 
-age1 = 15
-old = False
-ab = None
+# age1 = 15
+# old = False
+# ab = None
 
-print(type(name))
-print(type(age))
-print(type(price))
+# print(type(name))
+# print(type(age))
+# print(type(price))
 
-print(type(age1))
-print(type(old))
-print(type(ab))
+# print(type(age1))
+# print(type(old))
+# print(type(ab))
 
-# Small Code Print SUM
-a = 1000
-b = 500
-sum = a - b
-print(sum)
+# # Small Code Print SUM
+# a = 1000
+# b = 500
+# sum = a - b
+# print(sum)
+
+# arithmetic operators
+firstVal = 5
+secondVal = 2
+
+print(firstVal + secondVal)
+print(firstVal - secondVal)
+print(firstVal * secondVal)
+print(firstVal / secondVal)
+print(firstVal % secondVal) # remainder
+print(firstVal ** secondVal) # power operator ---> firstVal^secondVal e.g: --> 5^2
+
+# Relational / Comparison Operators
+a = 50
+b = 20
+
+print(a == b) # Obviously False bcz a is not equal to b
+print(a != b) # True bcz the statment is correct a!=b a is not equal to b.
+print(a >= b) # True bcz a is greater than b and that right statment
+print(a > b) # True
+print(a <= b) # False bcz a is less than equal to b
+print(a < b)  # False
