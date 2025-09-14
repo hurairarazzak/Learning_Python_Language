917d9a8e8452ea21af44869b3e407bace3b80891
hurairarazzak <hurairarazzak125@gmail.com>
2025-09-14 23:59:53 +0500

    learn slicing in python

--- lecture_2/lecture_2.py
+++ lecture_2/lecture_2.py
@@ -22,6 +22,24 @@ print(indexingStr[0])
 # Error bcz in python we can only
 # access character but we can't manipulate character 
 # we can't modify them
-str = "Razzak"
-str[5] = "q"
-print(str) 
+# str = "Razzak"
+# str[5] = "q"
+# print(str)
+
+# Slicing in Python
+sliStr = "HurairaRazzak"
+print(sliStr[1:4]) 
+print(sliStr[:4]) 
+
+# str[ starting_idx : ending_idx ] #ending idx is not included
+# Accessing parts of a string
+
+# str =
+
+# “ApnaCollege”
+
+# str[ 1 : 4 ] is “pna”
+
+# str[ : 4 ] is same as str[ 0 : 4]
+
+# str[ 1 : ] is same as str[ 1 : len(str) ]
