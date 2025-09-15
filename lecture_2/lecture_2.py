acd554749e5920b5ffe40909190ecd674e5f3a1c
hurairarazzak <hurairarazzak125@gmail.com>
2025-09-15 23:59:32 +0500

    learned Conditional Statements

--- lecture_2/lecture_2.py
+++ lecture_2/lecture_2.py
@@ -36,3 +36,73 @@ print(sliStr[5:len(sliStr)]) # len(sliStr) iska mtlb 5 length ke baad string ke 
 # Slicing Negative Indexing
 fruitStr = "Apple"
 print(fruitStr[-3:-1])
+
+# String Functions
+funcStr = "i'm studying python from youtube."
+print(funcStr.endswith("youtube")) #returns true if string ends with substr
+
+# print(str.capitalize(funcStr)) #capitalizes 1st char and make new string
+funcStr = str.capitalize(funcStr)
+print(funcStr) 
+
+print(funcStr.replace("python", "javascript")) # This will replace the python word to javascript word
+ 
+print(funcStr.find("u")) # So this will find the index number of first 
+# written character "u" in  the string
+
+print(funcStr.count("y")) # this means letter y kitni baar exist karta hae 
+# hamare string mae tou yae utne number btayega apko kae itni bar "y" hae
+# string mae
+
+# Conditional Statements
+# if-elif-else (SYNTAX)
+
+# age = 21
+# if(age >= 18):
+#     print("Can vote")
+#     print("Can drive")
+
+light = "red"
+
+if(light == "red"):
+    print("Stop your car")
+elif(light == "green"):
+    print("You can go now")
+elif(light == "yellow"):
+    print("Look")
+else:
+    print("Light is broken") # The 4 tab space is called indentation
+
+print("end of code")
+
+# Conditional Statements
+# Grade students based on marks
+
+# marks >= 90, grade = "A"
+# 90 > marks >= 80, grade = "B"
+# 80 > marks >= 70, grade = "C"
+# 70 > marks, grade = "D"
+
+marks = int(input("Enter you marks: "))
+
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 80 and marks < 90):
+    grade = "B"
+elif(marks >= 70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade of the Student:", grade)
+
+# Nesting in Conditional Statements
+age = int(input("Enter Your Age: "))
+if(age >= 18):
+    if(age >= 80):
+        print("Too Old Cannot Drive")
+    else:
+        print("Can Drive")
+else:
+    print("Too Young Cannot Drive")
+
---
+++ lecture_2/practice_2.py
@@ -0,0 +1,43 @@
+# Let‘s Practice
+# WAP to input user’s first name & print its length.
+# WAP to find the occurrence of ‘$’ in a String.
+
+# 1) Question ka code
+# userName = str(input("Enter Your First Name: "))
+# print("Your Name length is:", len(userName))
+
+# 2) Question ka code
+# occurrenceStr = "Hey $, it's a pleasure to meeting with you $, Thanks $"
+# print("There are",occurrenceStr.count("$"), "$ sign in string")
+
+# Let‘s Practice
+# WAP to check if a number entered by the user is odd or even.
+# WAP to find the greatest of 3 numbers entered by the user.
+# WAP to check if a number is a multiple of 7 or not.
+
+# 1) Question ka code 
+# chkNum = int(input("Enter the number to check odd or even: "))
+# if(chkNum % 2 == 0):
+#     print("Your number is even")
+# else:
+#     print("Your number is odd")
+
+# 2) Question ka code
+a = int(input("Enter your a number: "))
+b = int(input("Enter your b number: "))
+c = int(input("Enter your c number: "))
+
+if(a >= b and a >= c):
+    print("A is the big number in all")
+elif(b >= a and b >= c):
+    print("B is the big number in all")
+else:
+    print("C is the big number in all")
+
+# 3) Question ka code
+x = int(input("enter the number: "))
+
+if(x % 7 == 0):
+    print("multiple of 7")
+else:
+    print("not a multiple of 7")
