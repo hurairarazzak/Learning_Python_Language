91ca600b0016c262cd8f677a270e56cb4f13c3b8
hurairarazzak <hurairarazzak125@gmail.com>
2025-09-18 23:32:14 +0500

    lecture 3 started

---
+++ lecture_3/lecture_3.py
@@ -0,0 +1,17 @@
+# marks1 = 94.7
+# marks2 = 82.3
+# marks3 = 67.4
+# marks4 = 75.5
+# marks5 = 64.8
+# Agr is tareqe se 50 students ko add karna hoga tou 50 variable bnane honge
+# tou is tareqe se bht zyada variable bnane honge aur har var.. ko track karna hoga
+
+marks = [84.2, 76.1, 97.6, 85.4, 98.3]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[2])
+
+student = ["Ali", 84, "Karachi"] # we can also store different types of data
+print(student)
